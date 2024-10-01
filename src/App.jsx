aeb7bbd76e610351158e10c60b7277f18832f80f
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2024-10-01 01:38:46 +0100

    install paystack

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./Home/home";
 import Contact from "./Contact/contact";
 import AboutUs from "./About/about";
 import Join from "./Join/join";
+import Support from "./Support/support";
 
 const route = createBrowserRouter([
   {
@@ -21,6 +22,10 @@ const route = createBrowserRouter([
   {
     path:"/join",
     element:<Join/>
+  },
+  {
+    path:"/support",
+    element:<Support/>
   },
 
 ])
---
+++ src/Support/support.jsx
@@ -0,0 +1,10 @@
+
+
+import  "./style.scss";
+
+
+export default function Support(){
+    return <div>
+                <h3>Do You Want to Support the Association Financally? </h3>
+    </div>
+}
