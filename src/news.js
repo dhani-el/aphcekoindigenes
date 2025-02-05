128b4666af2137b715b6a880f0832603276b6b0b
ukuhor omotayo daniel <95970533+dhani-el@users.noreply.github.com>
2025-02-05 11:16:36 +0100

    Update news.js

--- src/news.js
+++ src/news.js
@@ -1,96 +1,148 @@
 
 
 export default [{
-    image:"iga-idungaran.png",
-    title:"random title",
-    description:"random desc",
-    author:"daniel",
-    date:"11:15:2024",
-    body:"total content of the news"
-},{
     image:"Lagosbridge.jpeg",
-    title:"Meet and greet",
-    description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae pariatur dolore possimus! Ex, dicta? Pariatur, quibusdam est! Excepturi, veniam deserunt.",
-    author:"Daniel Ukuhor",
-    date:"3 months ago",
-    body:`Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt, magnam quia.
-     Eveniet veniam sequi corporis dolorem, dolores aliquam iusto odio nam ea commodi assumenda esse 
-     minima similique facilis reprehenderit itaque labore ducimus eaque! Debitis ipsum explicabo, accusamus, 
-     minus porro corporis placeat optio ipsa fugiat exercitationem itaque nisi maiores nam. Illo, fugit id.
-    Consectetur odio veritatis accusamus hic rem corrupti numquam illo placeat distinctio cumque deleniti 
-    animi, aliquid nesciunt nihil fugit dolores velit odit soluta omnis, quia nisi, ullam temporibus blanditiis.
-    Sunt voluptatem ab libero id error aliquid hic, deleniti saepe modi blanditiis, obcaecati cumque sapiente? 
-    Qui in sint quod maxime. Dolore sit animi cumque ratione doloremque esse voluptatibus nulla maiores ullam!
-    Commodi nesciunt, ab, deserunt vitae nostrum molestiae ipsam temporibus ex itaque doloribus exercitationem 
-    excepturi harum tenetur necessitatibus molestias labore nisi vel. Temporibus corporis accusamus ut quaerat
-    tempore quidem, cupiditate quas, libero ipsum, cum sed pariatur voluptate enim dolorum eum?`
-},
-{
-    image:"Lagosbridge.jpeg",
-    title:"Meet and greet",
-    description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae pariatur dolore possimus! Ex, dicta? Pariatur, quibusdam est! Excepturi, veniam deserunt.",
-    author:"Daniel Ukuhor",
-    date:"3 months ago",
-    body:`Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt, magnam quia.
-     Eveniet veniam sequi corporis dolorem, dolores aliquam iusto odio nam ea commodi assumenda esse 
-     minima similique facilis reprehenderit itaque labore ducimus eaque! Debitis ipsum explicabo, accusamus, 
-     minus porro corporis placeat optio ipsa fugiat exercitationem itaque nisi maiores nam. Illo, fugit id.
-    Consectetur odio veritatis accusamus hic rem corrupti numquam illo placeat distinctio cumque deleniti 
-    animi, aliquid nesciunt nihil fugit dolores velit odit soluta omnis, quia nisi, ullam temporibus blanditiis.
-    Sunt voluptatem ab libero id error aliquid hic, deleniti saepe modi blanditiis, obcaecati cumque sapiente? 
-    Qui in sint quod maxime. Dolore sit animi cumque ratione doloremque esse voluptatibus nulla maiores ullam!
-    Commodi nesciunt, ab, deserunt vitae nostrum molestiae ipsam temporibus ex itaque doloribus exercitationem 
-    excepturi harum tenetur necessitatibus molestias labore nisi vel. Temporibus corporis accusamus ut quaerat
-    tempore quidem, cupiditate quas, libero ipsum, cum sed pariatur voluptate enim dolorum eum?`
-},{
-    image:"Lagosbridge.jpeg",
-    title:"Meet and greet",
-    description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae pariatur dolore possimus! Ex, dicta? Pariatur, quibusdam est! Excepturi, veniam deserunt.",
-    author:"Daniel Ukuhor",
-    date:"3 months ago",
-    body:`Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt, magnam quia.
-     Eveniet veniam sequi corporis dolorem, dolores aliquam iusto odio nam ea commodi assumenda esse 
-     minima similique facilis reprehenderit itaque labore ducimus eaque! Debitis ipsum explicabo, accusamus, 
-     minus porro corporis placeat optio ipsa fugiat exercitationem itaque nisi maiores nam. Illo, fugit id.
-    Consectetur odio veritatis accusamus hic rem corrupti numquam illo placeat distinctio cumque deleniti 
-    animi, aliquid nesciunt nihil fugit dolores velit odit soluta omnis, quia nisi, ullam temporibus blanditiis.
-    Sunt voluptatem ab libero id error aliquid hic, deleniti saepe modi blanditiis, obcaecati cumque sapiente? 
-    Qui in sint quod maxime. Dolore sit animi cumque ratione doloremque esse voluptatibus nulla maiores ullam!
-    Commodi nesciunt, ab, deserunt vitae nostrum molestiae ipsam temporibus ex itaque doloribus exercitationem 
-    excepturi harum tenetur necessitatibus molestias labore nisi vel. Temporibus corporis accusamus ut quaerat
-    tempore quidem, cupiditate quas, libero ipsum, cum sed pariatur voluptate enim dolorum eum?`
-},{
-    image:"Lagosbridge.jpeg",
-    title:"Meet and greet",
-    description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae pariatur dolore possimus! Ex, dicta? Pariatur, quibusdam est! Excepturi, veniam deserunt.",
-    author:"Daniel Ukuhor",
-    date:"3 months ago",
-    body:`Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt, magnam quia.
-     Eveniet veniam sequi corporis dolorem, dolores aliquam iusto odio nam ea commodi assumenda esse 
-     minima similique facilis reprehenderit itaque labore ducimus eaque! Debitis ipsum explicabo, accusamus, 
-     minus porro corporis placeat optio ipsa fugiat exercitationem itaque nisi maiores nam. Illo, fugit id.
-    Consectetur odio veritatis accusamus hic rem corrupti numquam illo placeat distinctio cumque deleniti 
-    animi, aliquid nesciunt nihil fugit dolores velit odit soluta omnis, quia nisi, ullam temporibus blanditiis.
-    Sunt voluptatem ab libero id error aliquid hic, deleniti saepe modi blanditiis, obcaecati cumque sapiente? 
-    Qui in sint quod maxime. Dolore sit animi cumque ratione doloremque esse voluptatibus nulla maiores ullam!
-    Commodi nesciunt, ab, deserunt vitae nostrum molestiae ipsam temporibus ex itaque doloribus exercitationem 
-    excepturi harum tenetur necessitatibus molestias labore nisi vel. Temporibus corporis accusamus ut quaerat
-    tempore quidem, cupiditate quas, libero ipsum, cum sed pariatur voluptate enim dolorum eum?`
-},{
-    image:"Lagosbridge.jpeg",
-    title:"Meet and greet",
-    description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae pariatur dolore possimus! Ex, dicta? Pariatur, quibusdam est! Excepturi, veniam deserunt.",
-    author:"Daniel Ukuhor",
-    date:"3 months ago",
-    body:`Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt, magnam quia.
-     Eveniet veniam sequi corporis dolorem, dolores aliquam iusto odio nam ea commodi assumenda esse 
-     minima similique facilis reprehenderit itaque labore ducimus eaque! Debitis ipsum explicabo, accusamus, 
-     minus porro corporis placeat optio ipsa fugiat exercitationem itaque nisi maiores nam. Illo, fugit id.
-    Consectetur odio veritatis accusamus hic rem corrupti numquam illo placeat distinctio cumque deleniti 
-    animi, aliquid nesciunt nihil fugit dolores velit odit soluta omnis, quia nisi, ullam temporibus blanditiis.
-    Sunt voluptatem ab libero id error aliquid hic, deleniti saepe modi blanditiis, obcaecati cumque sapiente? 
-    Qui in sint quod maxime. Dolore sit animi cumque ratione doloremque esse voluptatibus nulla maiores ullam!
-    Commodi nesciunt, ab, deserunt vitae nostrum molestiae ipsam temporibus ex itaque doloribus exercitationem 
-    excepturi harum tenetur necessitatibus molestias labore nisi vel. Temporibus corporis accusamus ut quaerat
-    tempore quidem, cupiditate quas, libero ipsum, cum sed pariatur voluptate enim dolorum eum?`
+    title:"History of the past and present Oba of Lagos.",
+    description:"Lagos dynasty since 1600.",
+    author:"PRINCE OLUSI ADEPEJU",
+    date:"2nd February 2025",
+    body:`ASHIPA (1600-1630)
+Ashipa was regarded as the founder of dynasty of Lagos. He was the son Oba of Benin. He was 
+acting as the leader of the people of Lagos but not as the king. He serves as the intermediary 
+between the Oba of Benin and the people of Lagos (eko),he spent 27 years as the leader of eko ,and 
+died in 1630 and his son Ado took over his responsibility.
+OBA ADO (1630-1669)
+Oba ado, the son of ashipa took over the leadership role played by his father in 1630 during his own 
+reign, he was elevated to an Oba and was crown as the king of Lagos by the Oba of Benin. Oba ado 
+became the first Oba of Lagos. Oba ado had three children,2 males and 1 female. They include 
+(Gabaro, Akinsemoyin, and Erelu Kuti).Oba ado ruled for 39years. He joined his ancestor in 1669 and 
+his first son Gabaro succeeded him.
+OBA GABARO (1669-1704)
+Oba Gabaro was installed in 1669 after his father death. During his reign, he moved the government 
+to the island in particular isale eko where the present iga Idungran is today.he was the first Oba to 
+erect a building in the palace, he also created the institution of chieftaincy and kingmakers.he had 
+only one child during his lifetime called Eletu kekere.Oba Gabaro died in 1904 and his brother 
+Akinsemoyin succeeded him.
+OBA AKINSEMOYIN (1704-1749)
+Oba Akinsemoyin started in own reign in 1704, it was during his reign that the Portuguese came to 
+Lagos as slave dealers. It was in records that the palace “iga idungran” was built during this period 
+with the assistance of the Portuguese . Oba Akinsemoyin never had a son but he has six female 
+children, he reign for 45 years and died in 1749.
+OBA ELETU-KEKERE (1749)
+Oba Eletu kekere succeeded Oba akinsemoyin(his uncle).Oba eletu kekere was the only son of Oba 
+Gabaro, during his reign, he had no child and died within the year he was installed as the 
+Oba.therefore ending the lineage of Oba gabaro. After his death, there was controversy on who to 
+succeed the throne, because Oba akinsemoyin had no son. Erelu kuti the sister of akinsemoyin is 
+also a female and it is not possible for a female to succeed the throne.The only option was to choose 
+the son of Erelu kuti to succeed the throne. Erelu kuti has two son’s (Ologun-kutere & sokun).The
+eldest son Ologun-kutere was chosen to succeed the throne.
+OBA OLOGUN-KUTERE(1749-1775)
+Oba Ologun kutere ascended the throne in 1949 after the death of his cousin eletu kekere. Oba 
+ologun kutere was a strong king, he won the respect of neighboring Oba by the way he handled a 
+threatening rebellion against paying tributes. He had 6 children “Eshinlokun, Adele, Akitoye, Olusi, 
+Akiolu, and Olukoya”.he joined his ancestor in 1775 and was succeeded by his son ADELE 
+AJOSUN(his son)
+OBA ADELE AJOSUN(1775-1780&1834)
+In 1775, when Oba Adele Ajosun succeeded his father, Lagos entered a period where the Oba had to 
+concern themselves with consolidating their position on the throne , his reign was rich with intrigues 
+and counter-intrigues between him and his brother ESHILOKUN. Oba Adele Ajosun was an indulgent 
+father who allowed his children to introduce “EGUNGUN” to the palace. This led to his dethronement 
+and he was banished in 1780 but was reinstated in 1852 after his brothers(ESHILOKUN, IDEWU 
+OJULARI had reigned. He died in 1834
+OBA ESHILOKUN(1780-1819)
+Oba Eshilokun came to the throne in 1780 following the dethronement of Oba Adele ajosun, though 
+his reign was uneventful, he successfully resisted the attempt by Adele to out him from the throne. He kept the throne till 1819 when he eventually joined his ancestors. He had 2 children(Idewu Ojulari 
+and Kosoko)
+OBA IDEWU OJULARI(1819-1832)
+By the time Oba idewu ojulari took over the throne, he was regarded as a popular king who became 
+so obsessed by avarice. He was given the advice to go exile or commit suicide, the traditional way of 
+treating undesirable king, but instead Ojulari decided to commit suicide. He died without an issue in 
+1832(childless)
+OBA OLUWOLE(18341841)
+Oba oluwole was the son of Adele ajosun, his succeeded the throne in 1834 after the death of his 
+father, unfortunately,for Oba oluwole, he died without an issue(childless), his death was as a result of 
+a terrible explosion of gun powder stored in the palace.
+OBA AKINTOYE(1841-1845&1851-1853)
+Oba akintoye is another son of ologun kutere, he succeeded the throne after his brother and nephews 
+had reign. Good intention backed by weakness made him to lose his throne the his nephew kosoko. 
+Despite the advices of his chiefs, Oba akintoye recalled kosoko so as to bring him back to fold . The 
+result was that he lost the throne and was able to get it back through the help of the help of the 
+British. In 1851 he signed the treaty to stop slavery
+OBA KOSOKO (1845-1851)
+Oba kosoko is a very strong man, he was very powerful spiritually.he became the Oba of Lagos in 
+1845 after plotting a coup against Oba Akitoye in 1845,his reign was warlike he believed he has 
+suffered injustice for not been appointed the Oba an 1841 then in 1845, he plotted a coup against his 
+uncle (Oba akintoye). During is reign, the British came to him to sign a treaty to stop slavery but Oba 
+kosoko insisted not sign the treaty, then Lagos was bombarded by the British navel and Oba akitoye 
+was reinstalled as the Oba in1851
+OBA DOSUNMU (1853-1885)
+In 1853, Oba dosunmu took over the reign of the throne of his forefather. He made durable 
+contribution to the development of Lagos and this gave a clear illustration of the lofty ideas of Obaship 
+which are in consonance with modern civilization, in august 1861 Oba dosunmu concluded the treaty 
+of cession of Lagos to the British government, Lagos became a freedom land and consequently many 
+traders started to bargain with overseas merchant for lawful and prosperous trade. and the various 
+constitutional developments helped further to turn Lagos into truly peaceful and prosperous Nigeria 
+city. He died in 1885
+OBA OYEKAN (1885-1900)
+Oba oyekan1 ascended the throne in 1885 though he did not enjoy good health during his reign but 
+carried on his duties till he joined his ancestors in 1900. He was succeeded by Oba eshugbayi eleko, 
+Oba oyekan died at the of 50 years of age.
+OBA ESUGBAYI (1901-1925&1932)
+The reign of Oba ESUGBAYI ELEKO started in 1901,he is popularly called as eleko, he succeeded 
+his brother oyekan1. But during his reign, it was full with British politics and wrangling,because of 
+British politics, he was sent on exile to Oyo kingdom by the then governor sir high Clifford in 1925. As 
+the custom of the people of Yoruba, anybody on exile is expected to be dead person, therefore the 
+council of chief marker therefore quickly elected another Oba, but during esugbayi exile, he did not 
+die and he returned to Lagos in 1931 and was reinstalled that same year. He died in 1932.
+OBA IBIKUNLE-AKITOYE(1925-1928)
+Oba ibikunle akitoye succeeded the throne after oba esugbayi was been sent on exile. But before his 
+installation, he was told by the priest that he will not last long on the throne that he will only spend 
+three years on the throne but ibikunle insisted on his installation that if he is to spend a day on the 
+throne , let it be, then his installation took place normally as the custom deserve. During his reign, he 
+was the first Christian oba of Lagos. Exactly three years as predicted, oba ibikunle akitoye joined his 
+ancestor. And he was succeeded by Oba sanusi Olusi.
+OBA SANUSI-OLUSI(1928-1931)
+In 1928, after the death of oba ibikunle akitoye, his cousin sanusi Olusi was chosen to succeed him. It 
+was in record that oba sanusi Olusi was the first Muslim oba of Lagos just as ibikunle was the first 
+Christian oba. During the reign of sanusi Olusi he broke the old pagan tradition by attending the 
+Muslim praying ground on the festival day in 1928, This action created enemies for him from the
+pagan group.Also during his reign,he created the relaxation space at the frontage of the palace which 
+Lagosian popularly called ESE-IGA,This formed a meeting/relaxation point for the Lagos of the 
+old.oba sanusi Olusi was a peace loving oba because in 1931, when oba esugbayi returned from 
+exile, he accepted to leave the throne for esugbayi to returned.this humble manner, was 
+compensated by the British government and another palace was built for him at OKE ARIN. This 
+palace is popularly called IGA-OLUSI.
+OBA FALOLU(1932-1949)
+Oba falolu ascended the throne 1932, but before his installation ,there was crisis on who to succeed 
+throne, some were in support of sanusi Olusi while some were in support of re-appointing new oba. 
+This crisis led to the setting of panel of inquiry that comprised of 6 traditional chiefs, 2chiefs out of 6 
+supported sanusi Olusi while 4 supported falolu. And oba falolu was declared the oba and was 
+installed immediately, oba fololu did not perform the traditional rite known as ETUTU-IJO (a traditional 
+rite after the death of a previous oba. But when it became an embarrassment,then he later comply 
+with the tradition. Oba falolu died in 1949 and was succeeded by adeniji Adele
+OBA ADENIJI ADELE(1949-1964)
+The reign of oba musendiku adeniji Adele started in 1949 following the death of oba falolu. Before he 
+became the oba he was a senior civil servant, he head the treasury department in kano.before and 
+during his reign he had crisis with the dosunmu/oyekan family. Because they believed that the throne 
+was only meant for members of their family only.but unfortunately for them, the court later the 
+declared that the throne his meant for members of the royal family but not only dosunmu/oyekan 
+family. Oba adeniji won in court, but in 1964 he joined his ancestor.And was succeeded by adeyinka 
+oyekan in 1965
+OBA ADEYINKA OYEKAN II
+Oba adeyinka oyekan II was installed in 1965, he was born in Lagos on 30th June 1910. He received 
+his early secondary education at eko boys high school, he later gained admission to kings college 
+Lagos. He was a man imbued with lofty ideas from his youth. He passed on to the school of 
+pharmacy, yaba, in 1930-1932 where obtained a diploma in pharmacy. During his reign as the Oba of 
+Lagos, he restored lost custom and tradition of the people of Lagos.
+Oba adeyinka oyekan was very honest,kind and has human feelings. He died at the age of 93 and 
+was succeeded by Oba Rilwan Akiolu
+OBA RILWAN-AKIOLU (2003-TO DATE)
+Oba rilwanu osuolanle aremu babatunde akiolu was installed as the Oba of Lagos in may 2003.
+He was born on 29 October, 1943. He attended ansar ud deen primary school and secondary school. 
+Then university of Lagos Akoka where he studied law. He started his police career as a cadet 
+inspector and rose to the position of assistant inspector general of police before his retirement in 
+march, 2002.
+He later settled down as a legal practitioner till he was appointed by the gods of the land of Lagos to 
+rule the people of EKO ILE.(Lagos) in 2003
+Copyright©2006`
 }]
